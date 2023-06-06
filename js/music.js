da9e0072cc4d98e9b75e157b3e47f4a2acb47edc
JordanGuimaraes <129178749+JordanGuimaraes@users.noreply.github.com>
2023-06-06 08:55:02 -0300

    Add files via upload

---
+++ js/music.js
@@ -0,0 +1,16 @@
+const Music = {
+  buttonPressAudio: new Audio(
+    'https://github.com/maykbrito/automatic-video-creator/blob/master/audios/button-press.wav?raw=true'
+  ),
+  kitchenTimer: new Audio(
+    'https://github.com/maykbrito/automatic-video-creator/blob/master/audios/kichen-timer.mp3?raw=true'
+  ),
+  bgAudio: new Audio(
+    'https://github.com/maykbrito/automatic-video-creator/blob/master/audios/bg-audio.mp3?raw=true'
+  ),
+  buttonPressFlorest: new Audio('../music/Floresta.wav'),
+  buttonPressRain: new Audio('../music/Chuva.wav'),
+  buttonPressHouse: new Audio('../music/Cafeteria.wav'),
+  buttonPressFire: new Audio('../music/Lareira.wav')
+}
+export default Music
